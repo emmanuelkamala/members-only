3d445492639188a365c68d67eb969f545c8ec9bf
emmanuelkamala <emmanuelkamala86@gmail.com>
2019-08-25 14:25:15 +0200

    Update user model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_094609) do
+ActiveRecord::Schema.define(version: 2019_08_25_095938) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
