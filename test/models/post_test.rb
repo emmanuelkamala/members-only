38fb27e6f039196a57f9935a334da9d9808302cb
emmanuelkamala <emmanuelkamala86@gmail.com>
2019-08-29 10:36:00 +0200

    Add more testing for post and user models

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,7 +2,38 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
 
-	# test "associations" do
-	# 	should belong_to :user
-	# end
+	def setup
+		@user = users(:braxton)
+		@post = @user.posts.build(title: "My post",
+															body: "This is a very short post")
+	end
+
+	test "post should be valid" do
+		assert @post.valid?
+	end
+
+	test "title should be present" do
+		@post.title = ""
+		assert_not @post.valid?
+	end	
+
+	test "body should be present" do
+		@post.body = ""
+		assert_not @post.valid?
+	end
+
+	test "title should not be too long" do
+		@post.title = "Walnut" * 50
+		assert_not @post.valid?
+	end
+
+	test "body should not be too long" do
+		@post.body = "a" * 2001
+		assert_not @post.valid?
+	end
+
+	test "post should have a user_id" do
+		@post.user_id = ""
+		assert_not @post.valid?
+	end
 end
