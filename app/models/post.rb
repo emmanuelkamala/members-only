5732cc118dd79429c5406cb59e8e5e4fdf746685
emmanuelkamala <emmanuelkamala86@gmail.com>
2019-10-07 16:17:25 +0300

    Modify rubocop configuration file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 	validates :title, presence: true, length: { maximum: 200 }
-	validates :body,  presence: true, length: { maximum: 2000 }
+	validates :body, presence: true, length: { maximum: 2000 }
 	validates :user_id, presence: true, uniqueness: true
 	belongs_to :user
 end
